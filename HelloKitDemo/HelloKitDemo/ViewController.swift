5a960048ff4add1115d2d23c8859e2ce3d6422f0
liushuai <liushuai@hnxaca.com>
2025-11-05 14:46:44 +0800

    feat(功能模块): 上传文件

---
+++ HelloKitDemo/HelloKitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloKitDemo
+//
+//  Created by yafei li on 2025/11/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloKitLibrary/Sources/HelloKitLibrary/HelloKitBinary.xcframework/ios-arm64/HelloKitBinary.framework/Headers/Hello.swift
@@ -0,0 +1,15 @@
+//
+//  Hello.swift
+//  HelloKitBinary
+//
+//  Created by yafei li on 2025/11/5.
+//
+
+
+import Foundation
+
+public struct Hello {
+    public static func say() -> String {
+        return "Hello from HelloKit! 🚀"
+    }
+}
