0e5d05e824012707ab335a6e74010bcaed3d4888
liushuai <liushuai@hnxaca.com>
2025-11-06 15:26:41 +0800

    feat(功能模块): 更新代码

--- HelloKit/Package.swift
+++ HelloKit/Package.swift
@@ -5,21 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "HelloKit",
+    platforms: [
+           .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "HelloKit",
-            targets: ["HelloKit","MySDK","HelloKitTests"])
+            targets: ["HelloKit"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HelloKit"),
+            name: "HelloKit",
+            dependencies: [.target(name: "HelloKitBinary")]),
         .binaryTarget(
-                   name:"MySDK",
-                   url:"https://github.com/OnePiece8887/HelloKit/blob/main/releases/download/1.0.0/HelloKitBinary.xcframework.zip",
-                   checksum:"7be068b53331929caeb0b1336f1e4f2e72b63980ffbb386eccf9386de7e36346"
+                   name:"HelloKitBinary",
+                   path: "HelloKitBinary.xcframework"
                ),
         .testTarget(
             name: "HelloKitTests",
--- HelloKit/Sources/HelloKit/HelloKit.swift
+++ HelloKit/Sources/HelloKit/HelloKit.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import HelloKitBinary
+
+func useSDK()  {
+    let result =  Hello.say()
+    print("useSDK方法获取result:",result)
+}
