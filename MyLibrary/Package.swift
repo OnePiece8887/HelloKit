63c1f95920ba9ef39b7ff0772c0e9c7a2bb5db60
liushuai <liushuai@hnxaca.com>
2025-11-06 14:50:40 +0800

    feat(功能模块): 更新

--- MyLibrary/Package.swift
+++ MyLibrary/Package.swift
@@ -10,21 +10,11 @@ let package = Package(
         .library(
             name: "MyLibrary",
             targets: ["MyLibrary"]),
-        .library(name: "MySDK", targets: ["MySDK"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyLibrary"),
-        .binaryTarget(
-            name:"MySDK",
-            url:"https://github.com/OnePiece8887/HelloKit/blob/main/releases/download/1.0.0/HelloKitBinary.xcframework.zip",
-            checksum:"7be068b53331929caeb0b1336f1e4f2e72b63980ffbb386eccf9386de7e36346"
-        ),
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]
-        ),
+            name: "MyLibrary"), 
     ]
 )
